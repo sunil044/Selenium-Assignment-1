0b1d3e6a3b75c1273b6ba6000a34a1e6bce70116
sunil044 <63278743+sunil044@users.noreply.github.com>
2020-05-29 17:40:19 +0530

    Add files via upload

---
+++ LessThan600.java
@@ -0,0 +1,65 @@
+import java.util.Iterator;
+import java.util.Set;
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class LessThan600 {
+
+	public static void main(String[] args) throws InterruptedException {
+		
+		System.setProperty("webdriver.chrome.driver", "//C:\\Users\\Prasad\\New folder\\chromedriver.exe");
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().deleteAllCookies();
+		
+		driver.get("https://www.flipkart.com/grocery-supermart-store?marketplace=GROCERY&otracker=nmenu_Grocery");
+		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+		driver.manage().timeouts().implicitlyWait(40, TimeUnit.SECONDS);
+		
+		driver.findElement(By.xpath("//a[@class='_3Ep39l _1zF5ew']")).click();
+		driver.findElement(By.xpath("//input[@class='_2zrpKA _1dBPDZ']")).sendKeys("9900530469");
+		driver.findElement(By.xpath("//div[@class='Km0IJL col col-3-5']//input[@class='_2zrpKA _3v41xv _1dBPDZ']")).sendKeys("FlipTest123");
+		
+		Thread.sleep(2000);
+		
+		driver.findElement(By.xpath("//button[@class='_2AkmmA _1LctnI _7UHT_c']")).click();
+		Thread.sleep(2000); 
+		driver.findElement(By.xpath("//input[@name='pincode']")).sendKeys("560068" + Keys.ENTER);
+		Thread.sleep(5000);
+		
+		driver.findElement(By.xpath("//input[@class='LM6RPg']")).sendKeys("dry fruits" + Keys.ENTER);
+		Thread.sleep(3000);
+		driver.findElement(By.xpath("//*[@title=\"Ziofit Afghani Dried Figs\"]")).click();
+		driver.manage().timeouts().pageLoadTimeout(30, TimeUnit.SECONDS);
+		
+		Set<String> windowIds = driver.getWindowHandles();
+		Iterator<String> itr = windowIds.iterator();
+		
+		String mainwindow = itr.next();
+		String childwindow = itr.next();
+		
+		driver.switchTo().window(childwindow);
+		
+		driver.findElement(By.xpath("//button[@class='_2AkmmA _2Npkh4 _2MWPVK _2K_ZRu']")).click();
+		Thread.sleep(2000);
+		driver.findElement(By.xpath("//button[@class='_2AkmmA _2Npkh4 _2MWPVK undefined _2K_ZRu']")).click();
+		
+		WebElement e = driver.findElement(By.xpath("//button[@class='_2AkmmA iwYpF9 _3OLJ5L _7UHT_c']"));
+
+		boolean actualValue = e.isEnabled();
+
+		if (actualValue)
+		       System.out.println("Button is enabled");
+		else
+			   
+		       System.out.println("Button is disabled Please " + driver.findElement(By.xpath("//div[@class='K9Gpat']")).getText());
+		
+		driver.quit();    Sunil@4768
+	}
+
+}
